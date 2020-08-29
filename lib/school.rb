f50aca795070c78de2381d307141eeba1f183053
NavPoint1 <foleym156@gmail.com>
2020-08-29 11:48:05 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if self.roster.any? { |key, value| key == grade }
+            self.roster.map { |key, value|
+                if key == grade
+                    value << name
+                end
+            }
+        else
+            self.roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        self.roster.each { |key, value|
+            if key == grade
+                return value
+            end
+        }
+    end
+
+    def sort
+        self.roster.each { |key, value|
+            value.sort!
+        }
+    end
+end
